1e366c24952fa8fe3954eaf15ed5e07349d983ec
DESKTOP-PPQMNSS\ksj87 <ksj8564@gmail.com>
2021-10-10 17:23:43 +0900

    feat: 6. 게임 진행 OutputView 작성

---
+++ src/main/java/racinggame/OutputView.java
@@ -0,0 +1,35 @@
+package racinggame;
+
+public class OutputView {
+	
+	public static void printWinner(String winnerName) {
+		System.out.println("최종 우승자는 "+winnerName+" 입니다.");
+	}
+	
+	public static void printError(String msg) {
+		System.out.println("[ERROR] : "+msg);
+	}
+	
+	public static void printCarNameInstruction() {
+		System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+	}
+	
+	public static void printAttemptCountInstruction() {
+		System.out.println("시도할 회수는 몇회인가요?");
+	}
+	
+	public static void printExcuteResultInstruction() {
+		System.out.println("실행 결과");
+	}
+	
+	
+	public static void printResultPerAttempt(String carName, int moveDistance) {
+		System.out.print(carName+" : ");
+		for(int i=0;i<moveDistance;i++) {
+			System.out.print("-");
+		}
+		System.out.println();
+	}
+	
+	
+}
