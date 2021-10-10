96130c1bc16a715e873d28b3566822d097eeb33b
DESKTOP-PPQMNSS\ksj87 <ksj8564@gmail.com>
2021-10-10 16:21:49 +0900

    fix: 차량명 5자 초과 유효성 체크 추가
    5자 초과 차량명 입력 시 Exception 발생

--- src/main/java/racinggame/Car.java
+++ src/main/java/racinggame/Car.java
@@ -1,14 +1,21 @@
 package racinggame;
 
 public class Car {
+	
+	private static final int VALID_NAME_LENGTH = 5;
 
 	private String name;
 	
-	public Car(String name) {		
+
+	public Car(String name) throws IllegalArgumentException{
+		if (name.length() > VALID_NAME_LENGTH) {
+			throw new IllegalArgumentException("[ERROR] 차량명은 5글자를 초과할 수 없습니다.");
+		}
 		this.name = name;
 	}
 
 	public String getName() {
 		return name;
-	}	
+	}
+
 }
---
+++ src/test/java/racinggame/GameTest.java
@@ -0,0 +1,29 @@
+package racinggame;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class GameTest {
+
+	private Game game;
+
+	@BeforeEach
+	void beforeEach() {
+		game = new Game("Ray,Benz,Tico");
+	}
+
+	@Test
+	void 자동차_이름_입력() {
+		assertEquals(game.getCarList().get(0).getName(), "Ray");
+		assertEquals(game.getCarList().get(1).getName(), "Benz");
+		assertThrows(IllegalArgumentException.class, () -> {
+			game = new Game("Ray,Benz,Tucson");
+		});
+
+	}
+
+
+}
